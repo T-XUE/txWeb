5aef1f196ba8b6f4981c8b5625a1ed872f31b00f
T-XUE <543810522@qq.com>
2017-09-01 14:18:48 +0800

    更新前端页面

--- src/com/springmvc/controller/IndexController.java
+++ src/com/springmvc/controller/IndexController.java
@@ -2,17 +2,28 @@ package com.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class IndexController {
 
     private final String vm_path = "/";
 
-    @RequestMapping("index.htm")
-    public String index(Model model) throws Exception {
-        model.addAttribute("tt","吉大师考虑就是发的块雷锋精神克难奋进");
+    @RequestMapping("/")
+    public String index(Model model, HttpServletRequest request, HttpServletResponse response) {
+        model.addAttribute("service", "index");
         return vm_path + "front/index.vm";
+    }
+
+    @RequestMapping("{service}.htm")
+    public String vacation(Model model, HttpServletRequest request, HttpServletResponse response,
+                           @PathVariable String service) {
+        model.addAttribute("service", service);
+        return vm_path + "front/"+service+".vm";
     }
 
 }
